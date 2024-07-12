b3070912683fd311988dae54bcb5e9d8baf458a7
lukedavids <lukedavids@gmail.com>
2024-07-11 22:47:14 -0500

    completed bookbot2

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ def main():
     total_word_count = count_words(text)
     total_character_count = count_characters(text)
     sort_dict = sort_character_dict(total_character_count)
-    print(f"{total_word_count} words found in your document")
-    print(sort_dict)
+    
+    print(f"---Begin report of {book_path}---")
+    print()
+    print(f"{total_word_count} words found in the document")
+    print()
+    
+    for dict in sort_dict:
+        print(f"The letter {dict["char"]} was found {dict["num"]} times")
+    
+    print()
+    print("---End report---")
 
 def read_file(path):
     with open(path) as f:
@@ -29,12 +38,21 @@ def count_characters(text):
     return char_dict
 
 def sort_character_dict(char_dict):
+    # This function filters out non alphabetical characters, creates a list of dictionaries and sorts the list 
+    
     list_of_dicts = []
+    filtered_char_dict = {}
 
     for key, value in char_dict.items():
+        if key.isalpha():
+            filtered_char_dict[key] = value
+    
+    for key, value in filtered_char_dict.items():
         list_of_dicts.append({"char": key, "num": value})
     
     list_of_dicts.sort(reverse=True, key=sort_on)
+    
     return list_of_dicts
-
+    
+        
 main()
